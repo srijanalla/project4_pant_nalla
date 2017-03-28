aec3d3ed4151460fe663719208cb23613b181c1a
DESKTOP-AIOGU8U\srijanalla <snalla@utexas.edu>
2017-03-27 20:31:45 -0500

    update 8.31

--- 02 Shiny/Access data.world Database.R
+++
@@ -1,8 +0,0 @@
-require("jsonlite")
-require("RCurl")
-
-# Leaving username and key in to avoid error during R Markdown knitting, especially if replicated. Normally would be left as uredacted and predacted and changed through the console.
-uredacted = 'srijanalla'
-predacted = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnNyaWphbmFsbGEiLCJpc3MiOiJhZ2VudDpzcmlqYW5hbGxhOjozMWY0MDM3Mi0xZmZlLTQ3ZDgtYTg1NS1hMjU2NmM2MmUxZGIiLCJpYXQiOjE0ODQ2OTcyNzMsInJvbGUiOlsidXNlcl9hcGlfd3JpdGUiLCJ1c2VyX2FwaV9yZWFkIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZX0.5q00lu9-0EIhCMalKDcH41Hsdy9vIlFZSr0_nbwvtNNSg4CqwCXQuib1q0Ub2LGd24rAjHZzsJaANO7mIUXvJw'
-
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5012/rest/native/?query="select * from unemp"')),httpheader=c(DB='jdbc:data:world:sql:srijanalla:s-17-dv-project-4', USER=uredacted, PASS=predacted, MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -1,12 +1,15 @@
 require(ggplot2)
 require(dplyr)
+require("jsonlite")
+require("RCurl")
 
-source("Access data.world Database.R")
+# Leaving username and key in to avoid error during R Markdown knitting, especially if replicated. Normally would be left as uredacted and predacted and changed through the console.
+uredacted = 'srijanalla'
+predacted = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnNyaWphbmFsbGEiLCJpc3MiOiJhZ2VudDpzcmlqYW5hbGxhOjozMWY0MDM3Mi0xZmZlLTQ3ZDgtYTg1NS1hMjU2NmM2MmUxZGIiLCJpYXQiOjE0ODQ2OTcyNzMsInJvbGUiOlsidXNlcl9hcGlfd3JpdGUiLCJ1c2VyX2FwaV9yZWFkIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZX0.5q00lu9-0EIhCMalKDcH41Hsdy9vIlFZSr0_nbwvtNNSg4CqwCXQuib1q0Ub2LGd24rAjHZzsJaANO7mIUXvJw'
 
-x = df[, "v_9"]
-y = df[, "v_6"]
-z = df[, "v_1"]
-df=data.frame(x,y,z)
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5012/rest/native/?query="select * from unemp"')),httpheader=c(DB='jdbc:data:world:sql:srijanalla:s-17-dv-project-4', USER=uredacted, PASS=predacted, MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+
+df=df %>% dplyr::rename(x=v_9, y=v_6, z=v_1)
 
 server <- function(input, output) {
   output$plot1 <- renderPlot({
